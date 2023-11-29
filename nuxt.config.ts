ff9914a18f7d70885269f476a6bd950ffb429af7
ap-19927 <allmannpatrick@gmail.com>
2023-11-29 01:27:07 +0000

    d

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  css: ['~/assets/css/main.css'],
-  nitro: {
-      "output": {
-          dir: 'output',
-          serverDir: 'output/server',
-          publicDir: 'output/public'
-      }
-  }
+  css: ['~/assets/css/main.css']
 })

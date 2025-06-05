4321f2c556a0004ea0e693a8dceb7a9e0687cf6a
ap-19927 <allmannpatrick@gmail.com>
2025-06-05 02:42:33 +0000

    remove linkedin and resume from contact page

--- components/LinkedInLink.vue
+++
@@ -1,7 +0,0 @@
-<template>
-<div class="w-fit">
-  <NuxtLink to="https://www.linkedin.com/in/ap19927/">
-    <img src="/img/In-Blue-Logo.png.original.png" alt="LinkedIn profile" class="h-10 w-10">
-  </NuxtLink>
-</div>
-</template>
